090113745e1040750ea1cc26174f7a278b1073c6
Ishtmeet Singh <ishtmeetsingh.cse18@itbhu.ac.in>
2020-06-08 16:02:22 +0000

    Add files via upload

---
+++ yetanothertry/src/main/java/com/sample/Message.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+public class Message {
+	public String message;
+	
+	public String getMessage() {
+        return this.message;
+    }
+ 
+    public void setMessage(String message) {
+    	//System.out.println("HI");
+        this.message = message;
+    }
+	
+}
